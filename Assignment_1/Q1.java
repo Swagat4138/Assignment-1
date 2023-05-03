76615c141fdec1a6dbb213b018b17b5ca4bae787
Swagat <swagatsourav2@gmail.com>
2023-05-03 23:46:11 +0530

    initial commit

---
+++ Assignment_1/Q1.java
@@ -0,0 +1,16 @@
+package Assignment_1;
+import java.util.Scanner;
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter positive integer larger than 2: ");
+        int x = sc.nextInt();
+        int count = 0;
+        while(x>2){
+            count++;
+            x/=2;
+        }
+        System.out.println("The number of times one must repeatedly divide this number by 2 before getting a value less than 2 is "+ count);
+        sc.close();
+    }
+}
