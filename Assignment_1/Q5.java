c4a0060812172bb6023f45fd549301ff062bb373
Swagat <swagatsourav2@gmail.com>
2023-05-10 23:48:02 +0530

    Assignment_1

---
+++ Assignment_1/Q5.java
@@ -0,0 +1,19 @@
+/*
+ * Write a java method to calculate the sum of digits of a given number until the number is a 
+single digit. The method signature is as follows.
+public static int sum_Of_Digits(int n)
+ */
+package Assignment_1;
+import java.util.Scanner;
+
+public static int sum_Of_Digits(int n){
+    int n = sc.nextInt();
+    return n;
+}
+public class Q5 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
