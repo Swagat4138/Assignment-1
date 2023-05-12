b01353a6f929dabb37df8e36f1c9658a90ab1943
Swagat <swagatsourav2@gmail.com>
2023-05-13 00:20:35 +0530

    Assignment_1

--- Assignment_1/Q5.java
+++ Assignment_1/Q5.java
@@ -6,14 +6,27 @@ public static int sum_Of_Digits(int n)
 package Assignment_1;
 import java.util.Scanner;
 
-public static int sum_Of_Digits(int n){
-    int n = sc.nextInt();
-    return n;
-}
 public class Q5 {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        int n = sc.nextInt();
+        System.out.println("Sum of digits of "+n +"until the number is a single digit is "+ sum_0f_Digits(n));
+        sc.close();
+    }
+
+    public static int sum_0f_Digits(int n) {
+        int sum= 0;
+        while(n>0){
+            int r = n%10;
+            sum+=r;
+            n/=10;
+        }
+        if(sum>=10){
+            return sum_0f_Digits(sum);
+        }
+        else{
+            return sum;
+        }
     }
 }
